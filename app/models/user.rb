6734d48cf5f2662dfb2485e3e16b627042e247a9
yyudaii <yudai200009089311@gmail.com>
2024-07-04 14:48:03 +0900

    RSpecの導入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
-  validates :password
-  end
-        
   validates :nickname, presence: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
   validates :name, presence: true
   validates :myouji, presence: true
   validates :name_kana, presence: true
