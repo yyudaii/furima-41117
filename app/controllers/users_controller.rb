252cbdba8069150b20648418629edd556833681d
yyudaii <yudai200009089311@gmail.com>
2024-07-04 14:29:28 +0900

    ログインの設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,22 @@
 class UsersController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
 
   def edit 
   end
 
   def update
-    curren_user.update(user_params)
+
+
   end
 
 
   private
 
-  def user_params
-    params.require(:user).permit(:name,:email)
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,12 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } do
+  validates :password
+  end
+        
   validates :nickname, presence: true
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i}
   validates :name, presence: true
   validates :myouji, presence: true
   validates :name_kana, presence: true
