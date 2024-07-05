f9c7299b09b7ac884ae6507c5eef0e7486f99888
yyudaii <yudai200009089311@gmail.com>
2024-07-05 12:19:44 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,10 @@
 class ItemsController < ApplicationController
   def index 
   end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
+  end
 end
