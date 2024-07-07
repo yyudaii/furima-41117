45874a46766ae7c0de2ad996e7cfbcc7e5bcb346
yyudaii <yudai200009089311@gmail.com>
2024-07-07 14:22:45 +0900

    テストコードの修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -5,8 +5,6 @@ FactoryBot.define do
     message{'テスト用なんだから何でもいいだろ。別にさ'}
     price{'10000'}
 
-    after(:build) do |item|
-      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
-    end
+
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  it "nameが空では登録できない" do
+    @item.name = ''
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Name can't be blank")
+  end
+  it "messageが空では登録できない" do
+    @item.message = ''
+    @item.valid?
+    expect(@item.errors.full_messages).to include("Message can't be blank")
+  end
 end
