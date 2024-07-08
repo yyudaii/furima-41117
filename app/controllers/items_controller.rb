288c94ab40f5929cb42f7b58b79243ea6313d73a
yyudaii <yudai200009089311@gmail.com>
2024-07-08 21:00:31 +0900

    activehashの内容が反映されるように修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
   
   def index 
-    @itemn = Item.all
+    @items = Item.all
     #@items = Item.order("created_at DESC")
   end
 
