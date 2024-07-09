6cfee0049c15a0258ca43d07a562a362108ace72
yyudaii <yudai200009089311@gmail.com>
2024-07-09 18:36:39 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update,:destroy]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :ryakushiki_eisyou, only: [:show, :edit, :update, :destroy]
   before_action :jikatobi_kinshi, only: [:edit, :update, :destroy]
 
