f13e60cf03e3e1c68b4f020b82eaadbf60a9d958
yyudaii <yudai200009089311@gmail.com>
2024-07-09 11:27:59 +0900

    リネームしたマイグレーションファイルのコメントアウト

--- db/migrate/20240705080748_change_column_name.rb
+++ db/migrate/20240705080748_change_column_name.rb
@@ -1,9 +1,9 @@
 class ChangeColumnName < ActiveRecord::Migration[7.0]
   def change
-    rename_column :items, :category, :category_id
-    rename_column :items, :condition, :condition_id
-    rename_column :items, :shipping_fee, :shipping_fee_id
-    rename_column :items, :area, :area_id
-    rename_column :items, :delay, :delay_id
+    #rename_column :items, :category, :category_id
+    #rename_column :items, :condition, :condition_id
+    #rename_column :items, :shipping_fee, :shipping_fee_id
+    #rename_column :items, :area, :area_id
+    #rename_column :items, :delay, :delay_id
   end
 end
