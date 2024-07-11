74fc3ee5913e92b25d78ff2bac596372e77d37f2
yyudaii <yudai200009089311@gmail.com>
2024-07-11 16:06:19 +0900

    テストコードの記述

--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,5 +1,42 @@
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '商品が購入出来る時' do
+    it '全ての値が適切に入力されていれば保存できる' do
+      expect(@po).to be_valid
+    end
+  end
+  
+
+  context '商品が購入できない時' do
+    it '郵便番号が紐付いていなければ投稿できない' do
+      @po.post = nil
+      @po.valid?
+      expect(@po.errors.full_messages).to include('Post must exist')
+    end
+
+    it '市区町村が紐付いていなければ投稿できない' do
+      @po.area = nil
+      @po.valid?
+      expect(@po.errors.full_messages).to include('Area_id must exist')
+    end
+
+    it '番地が紐付いていなければ投稿できない' do
+      @po.address = nil
+      @po.valid?
+      expect(@po.errors.full_messages).to include('City must exist')
+    end
+
+    it '電話番号が紐付いていなければ投稿できない' do
+      @po.tel = nil
+      @po.valid?
+      expect(@po.errors.full_messages).to include('Tel must exist')
+    end
+
+    it 'ユーザーが紐付いていなければ投稿できない' do
+      @po.post = nil
+      @po.valid?
+      expect(@po.errors.full_messages).to include('Post must exist')
+    end
+  end
 end
