adbdb342f79075eee7cb5f284067cf772e29dcfb
yyudaii <yudai200009089311@gmail.com>
2024-07-15 17:52:06 +0900

    ordersテーブルからprefecture_idの削除

--- db/migrate/20240711091428_remove_prefecture_id_from_orders.rb
+++
@@ -1,5 +0,0 @@
-class RemovePrefectureIdFromOrders < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :orders, :prefecture_id, :integer
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_11_091428) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_11_034720) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -59,6 +59,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_07_11_091428) do
     t.datetime "updated_at", null: false
     t.integer "user_id"
     t.integer "item_id"
+    t.integer "prefecture_id"
   end
 
   create_table "purchases", charset: "utf8", force: :cascade do |t|
